176807d980fa775058b92f05571d392be72160e0
Jason <jasonbrito98@gmail.com>
2018-11-22 13:18:03 -0500

    Implement initial routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {GameComponent} from './game/game.component';
+import {GameStartComponent} from './game-start/game-start.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: GameComponent},
+  { path: 'play', component: GameStartComponent },
+  { path: 'game', component: GameComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
