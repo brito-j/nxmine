32f955d51608e0f129f4265b274695425a5967cd
Jason <jasonbrito98@gmail.com>
2018-11-25 14:22:05 -0600

    Document App Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {GameComponent} from './game/game.component';
 import {GameStartComponent} from './game-start/game-start.component';
 import {HomeComponent} from './home/home.component';
@@ -7,16 +7,17 @@ import {StatisticsComponent} from './statistics/statistics.component';
 import {HowToPlayComponent} from './how-to-play/how-to-play.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent},
-  { path: 'play', component: GameStartComponent },
-  { path: 'game', component: GameComponent},
-  { path: 'statistics', component: StatisticsComponent},
-  { path: 'how-to-play', component: HowToPlayComponent}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'play', component: GameStartComponent},
+  {path: 'game', component: GameComponent},
+  {path: 'statistics', component: StatisticsComponent},
+  {path: 'how-to-play', component: HowToPlayComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
